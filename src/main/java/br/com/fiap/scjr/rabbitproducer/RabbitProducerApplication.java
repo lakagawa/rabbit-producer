21038aefadaaac33950da2c48dfa402c3ed66fb6
lalakagawa <lalakagawa@hotmail.com>
2022-11-07 23:09:46 -0300

    producer

---
+++ src/main/java/br/com/fiap/scjr/rabbitproducer/RabbitProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.scjr.rabbitproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitProducerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/fiap/scjr/rabbitproducer/controllers/MessageController.java
@@ -0,0 +1,20 @@
+package br.com.fiap.scjr.rabbitproducer.controllers;
+
+import br.com.fiap.scjr.rabbitproducer.senders.QueueSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+    @Autowired
+    private QueueSender sender;
+
+    @GetMapping("/message")
+    public String send() {
+        sender.send("minha msf aqui :D");
+        return  "Deu certo";
+    }
+}
